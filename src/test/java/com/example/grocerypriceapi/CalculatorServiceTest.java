d148d444bb688eee8cab85c95a2a8d1be802c05a
Anthony Ikeda <anthony.ikeda@thoughtworks.com>
2023-10-16 08:34:10 -0500

    Trying to configure the swagger auth

--- src/test/java/com/example/grocerypriceapi/CalculatorServiceTest.java
+++ src/test/java/com/example/grocerypriceapi/CalculatorServiceTest.java
@@ -3,11 +3,8 @@ package com.example.grocerypriceapi;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
 import java.util.List;
